24602ec708543a52c4c64ebc06a808eee46be685
Jozef Kubis <kubis.jozef@outlook.com>
2024-05-21 10:34:33 +0200

    Uprava textu v polickach input

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -8,22 +8,22 @@ const SearchForm = ({
   setMaxCarbs,
 }) => {
   const printOnPg = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (searchItem) {
       setFoundItems((foundItems) => {
-        return [...foundItems, searchItem];
-      });
+        return [...foundItems, searchItem]
+      })
 
-      const form = document.querySelector(".form");
-      form.classList.add("height");
+      const form = document.querySelector(".form")
+      form.classList.add("height")
 
-      const header = document.querySelector(".header");
-      header.classList.add("quote");
+      const header = document.querySelector(".header")
+      header.classList.add("quote")
 
-      setSearchItem("");
+      setSearchItem("")
     }
-  };
+  }
 
   return (
     <div className="form flex flex-col items-center justify-center w-full h-screen fixed bg-[#1e1d2f] z-10">
@@ -31,7 +31,7 @@ const SearchForm = ({
         <div className="flex items-center justify-center space-x-4">
           <div className="flex space-x-4">
             <input
-              className="border-solid border-2 border-pink-600 rounded-md outline-none text-black"
+              className="border-solid border-2 border-pink-600 rounded-md outline-none text-black w-64 p-2 text-xl font-semibold"
               type="text"
               placeholder="Input Ingredient"
               value={searchItem}
@@ -40,7 +40,7 @@ const SearchForm = ({
             <label htmlFor="minProteinInput">Min Protein:</label>
             <input
               id="minProteinInput"
-              className="border-solid border-2 border-pink-600 w-12 rounded-md outline-none  text-black"
+              className="border-solid border-2 border-pink-600 w-16 rounded-md outline-none  text-black text-xl font-semibold"
               value={minProtein}
               onChange={(e) => setMinProtein(e.target.value)}
               type="number"
@@ -48,7 +48,7 @@ const SearchForm = ({
             <label htmlFor="maxCarbsInput">Max Carbs:</label>
             <input
               id="maxCarbsInput"
-              className="border-solid border-2 border-pink-600 w-12 rounded-md outline-none  text-black"
+              className="border-solid border-2 border-pink-600 w-16 rounded-md outline-none  text-black text-xl font-semibold"
               value={maxCarbs}
               onChange={(e) => setMaxCarbs(e.target.value)}
               type="number"
@@ -66,7 +66,7 @@ const SearchForm = ({
         "Quality recipes for every occasion"
       </h1>
     </div>
-  );
-};
+  )
+}
 
-export default SearchForm;
+export default SearchForm
